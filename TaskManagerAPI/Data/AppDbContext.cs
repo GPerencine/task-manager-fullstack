84e4040e3868ff46aef7c3558db91a641e8e70a2
Perencine <gabrielperencine1@gmail.com>
2026-02-24 17:09:18 -0300

    Fix: Data Base

--- TaskManagerAPI/Data/AppDbContext.cs
+++ TaskManagerAPI/Data/AppDbContext.cs
@@ -1,14 +1,12 @@
-using Microsoft.EntityFrameworkCore;
-using TaskManagerAPI.Models;
-
-namespace TaskManagerAPI.Data;
-
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
-    {
-    }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+    // Força o EF a usar o nome exato da tabela com aspas
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskItem>().ToTable("Tasks");
+    }
 }
