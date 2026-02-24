5f84523be461df06756e608636a65dcf32b4a224
Perencine <gabrielperencine1@gmail.com>
2026-02-24 16:14:41 -0300

    Fix: Ajustando caminho do publish no Dockerfile

--- TaskManagerAPI/Data/AppDbContextFactory.cs
+++ TaskManagerAPI/Data/AppDbContextFactory.cs
@@ -9,7 +9,6 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
 
-      
         var connectionString = "Host=15.228.44.113;Port=5432;Database=postgres;Username=postgres;Password=EhAA*$FPUm3uC7k;SSL Mode=Prefer;Trust Server Certificate=true;";
         
         optionsBuilder.UseNpgsql(connectionString);
